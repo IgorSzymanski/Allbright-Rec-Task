ed446c638812d89fdf3ad573d709b0b467de8eaa
Igor Szymański <igor.szymanski@me.com>
2020-02-12 02:33:51 +0100

    Commented out rate limiter due to a bug.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,26 @@ import { CommentsModule } from './comments/comments.module'
 import { FeedbackModule } from './feedback/feedback.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import * as OrmConfig from './orm.config'
-import { RateLimiterModule, RateLimiterInterceptor } from 'nestjs-rate-limiter'
-import { APP_INTERCEPTOR } from '@nestjs/core'
+import { RateLimiterModule } from 'nestjs-rate-limiter'
+// import { APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
 	imports: [
 		TypeOrmModule.forRoot(OrmConfig),
 		CommentsModule,
 		FeedbackModule,
-		RateLimiterModule,
+		RateLimiterModule.register({
+			keyPrefix: 'alb',
+			type: 'Memory',
+		}),
 	],
 	controllers: [AppController],
 	providers: [
 		AppService,
-		{
-			provide: APP_INTERCEPTOR,
-			useClass: RateLimiterInterceptor,
-		},
+		// {
+		// 	provide: APP_INTERCEPTOR,
+		// 	useClass: RateLimiterInterceptor,
+		// },
 	],
 })
 export class AppModule {}
